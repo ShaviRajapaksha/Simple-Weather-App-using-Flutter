bb45c621cef3d5ec633784c84ec815c7d4be04f4
IT20272654 <shavindurajapaksha@gmail.com>
2025-12-26 15:28:24 +0530

    feat: Implement weather fetching and display in WeatherPage

--- lib/pages/weather_page.dart
+++ lib/pages/weather_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:simple_weather_app_using_flutter/services/weather_service.dart';
+
+import '../models/weather_model.dart';
 
 class WeatherPage extends StatefulWidget {
   const WeatherPage({super.key});
@@ -8,8 +11,47 @@ class WeatherPage extends StatefulWidget {
 }
 
 class _WeatherPageState extends State<WeatherPage> {
+  //api key
+  final _weatherService = WeatherService('YOUR_API_KEY_HERE');
+  Weather? _currentWeather;
+
+  //fetch weather
+  _fetchWeather() async {
+    String cityName = await _weatherService.getCurrentCity();
+    try {
+      final weather = await _weatherService.getWeather(cityName);
+      setState(() {
+        _currentWeather = weather;
+      });
+    } catch (e) {
+      // Handle error, e.g., show a snackbar or dialog
+      print('Error fetching weather: $e');
+    }
+  }
+  //weather animation
+
+  //init state
+  @override
+  void initState() {
+    super.initState();
+    _fetchWeather();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //city name
+            Text(_currentWeather?.cityName ?? 'loading city...'),
+
+            //temperature
+            Text('${_currentWeather?.temperature.round()} °C'),
+          ],
+        ),
+      ),
+    );
   }
 }
