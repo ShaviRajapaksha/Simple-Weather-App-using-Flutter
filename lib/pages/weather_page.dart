e99d9f026a39534bf1c3a69eedaff0349bd7c34e
IT20272654 <shavindurajapaksha@gmail.com>
2025-12-26 21:10:59 +0530

    feat: Add weather animations using Lottie for different weather conditions

--- lib/pages/weather_page.dart
+++ lib/pages/weather_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 import 'package:simple_weather_app_using_flutter/services/weather_service.dart';
 
 import '../models/weather_model.dart';
@@ -13,7 +14,7 @@ class WeatherPage extends StatefulWidget {
 class _WeatherPageState extends State<WeatherPage> {
   //api key
   //website: https://home.openweathermap.org/api_keys
-  final _weatherService = WeatherService('YOUR_API_KEY_HERE');
+  final _weatherService = WeatherService('api key');
   Weather? _currentWeather;
 
   //fetch weather
@@ -29,7 +30,30 @@ class _WeatherPageState extends State<WeatherPage> {
       print('Error fetching weather: $e');
     }
   }
+
   //weather animation
+  String _getWeatherAnimation(String mainCondition) {
+    switch (mainCondition.toLowerCase()) {
+      case 'clear':
+        return 'assets/Weather-sunny.json';
+      case 'thunderstorm':
+        return 'assets/Weather-stormy.json';
+      case 'rain':
+      case 'drizzle':
+      case 'shower rain':
+        return 'assets/Weather-rainy.json';
+      case 'windy':
+      case 'clouds':
+      case 'fog':
+      case 'haze':
+      case 'mist':
+      case 'smoke':
+      case 'dust':
+        return 'assets/Weather-windy.json';
+      default:
+        return 'assets/Weather-sunny.json';
+    }
+  }
 
   //init state
   @override
@@ -47,6 +71,11 @@ class _WeatherPageState extends State<WeatherPage> {
           children: [
             //city name
             Text(_currentWeather?.cityName ?? 'loading city...'),
+
+            //animations
+            Lottie.asset(
+              _getWeatherAnimation(_currentWeather?.mainCondition ?? ''),
+            ),
 
             //temperature
             Text('${_currentWeather?.temperature.round()} °C'),
